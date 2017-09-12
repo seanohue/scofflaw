d67c5b1de47803d42f29226bdce2cdd2452b88f9
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-09-12 12:17:44 -0400

    Name change for components. Change name of project from scoundrel to scofflaw. Strip unused stuff from components.

---
+++ src/components/MapView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main-container">
+    <h1 class="incompatible-error-msg" v-if="!isROTCompatible">ROT is not supported by your browser.</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'map',
+  data () {
+    if (this.env === 'development') {
+      console.log('ROT:', this.ROT)
+    }
+    return {
+      isROTCompatible: this.ROT.isSupported()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 
-import * as ROTModule from './vendor/rot.min.js'
+import * as ROTModule from '@/vendor/rot.min.js'
 
-import App from './App'
-import router from './router'
+import App from '@/App'
+import router from '@/router'
 
 Vue.config.productionTip = false
 
 Vue.prototype.ROT = ROTModule.ROT
+Vue.prototype.env = process.env.NODE_ENV
 
 /* eslint-disable no-new */
 new Vue({
