b3d45acaf59ea87b28874ecd9bcfacdd4d172a8a
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-09-30 17:09:19 -0400

    Basic tutorial implementation of drawing a map.

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -13,14 +13,39 @@ export default {
       console.log('ROT:', this.ROT)
     }
     return {
+      map: {},
       isROTCompatible: this.ROT.isSupported()
     }
   },
 
   mounted () {
-    // Create and append the ROT map display.
-    this.display = new this.ROT.Display()
-    this.$el.appendChild(this.display.getContainer())
+    this.createDisplay()
+  },
+
+  methods: {
+    createDisplay () {
+      this.display = new this.ROT.Display()
+      this.$el.appendChild(this.display.getContainer())
+      this.buildMap()
+    },
+
+    // TODO: Pull into own class & persist using VueX.
+    buildMap () {
+      const digger = new this.ROT.Map.Digger()
+      digger.create((x, y, value) => {
+        if (value) return
+        // Ugh, gross.
+        this.map[x + ',' + y] = '.'
+      })
+      this.drawMap()
+    },
+
+    drawMap () {
+      Object.keys(this.map).forEach(key => {
+        const [x, y] = key.split(',').map((n) => parseInt(n, 10))
+        this.display.draw(x, y, this.map[key])
+      })
+    }
   }
 }
 </script>
